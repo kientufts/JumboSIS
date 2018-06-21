6d0bcbc1620d946caabf381d14299a4417e43a63
Kientufts <letrungkien1991@gmail.com>
2018-06-21 13:05:56 -0700

    student model class

---
+++ src/Student/StudentModel.java
@@ -0,0 +1,48 @@
+// a table mode to display the student images in jtable
+package Student;
+
+import javax.swing.Icon;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author kienle
+ */
+public class StudentModel extends AbstractTableModel {
+
+    private String[] columns;
+    private Object[][] rows;
+
+    public StudentModel() {
+    }
+
+    public StudentModel(Object[][] data, String[] columnsName) {
+        this.columns = columnsName;
+        this.rows = data;
+    }
+
+    public Class getColumnClass(int col) {
+        // the index of the image column is 8
+        if (col == 8) {
+            return Icon.class;
+        } else {
+            return getValueAt(0, col).getClass();
+        }
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.rows.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return this.rows[rowIndex][columnIndex];
+    }
+
+}
